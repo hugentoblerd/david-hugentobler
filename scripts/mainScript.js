1a41f11a196f806655b87bf3d8f54f72b97acbdd
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-26 16:19:32 -0600

    adds bootstrap javascript and function for navbar (.tab())

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,12 +1,9 @@
 $(document).ready(function () {
 
-  $('#nav-menu > li').on('click',function() {
-    var id = $(this > 'a').attr('href');
-    $(this).siblings().removeClass('active'); 
-    $(this).addClass('active');
-    // $($(this > 'a').attr('href')[0]).siblings().removeClass('active in');
-    $(id[0]).addClass('active in');
-  });
+$('#nav-menu a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
 
 
 
